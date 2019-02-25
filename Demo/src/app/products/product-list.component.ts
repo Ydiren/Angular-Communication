7d0acbb9d81b6b0339214e364f7953a59e185791
Andy <andymg@gmail.com>
2019-02-25 17:08:57 +0000

    property bag service (module 7)

--- Demo/src/app/products/product-list.component.ts
+++ Demo/src/app/products/product-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 import { CriteriaComponent } from '../shared/criteria/criteria.component';
+import { ProductParameterService } from './product-parameter.service';
 
 @Component({
     templateUrl: './product-list.component.html',
@@ -11,10 +12,8 @@ import { CriteriaComponent } from '../shared/criteria/criteria.component';
 export class ProductListComponent implements OnInit, AfterViewInit {
 
     pageTitle: string = 'Product List';
-    showImage: boolean;
     includeDetail: boolean = true;
 
-    @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
     parentListFilter: string;
 
     imageWidth: number = 50;
@@ -24,7 +23,17 @@ export class ProductListComponent implements OnInit, AfterViewInit {
     filteredProducts: IProduct[];
     products: IProduct[];
 
-    constructor(private productService: ProductService) {
+    @ViewChild(CriteriaComponent) filterComponent: CriteriaComponent;
+
+    get showImage(): boolean {
+      return this.productParameterService.showImage;
+    }
+    set showImage(value: boolean) {
+      this.productParameterService.showImage = value;
+    }
+
+    constructor(private productService: ProductService,
+                private productParameterService: ProductParameterService) {
 
     }
 
@@ -37,13 +46,14 @@ export class ProductListComponent implements OnInit, AfterViewInit {
       this.productService.getProducts().subscribe(
           (products: IProduct[]) => {
               this.products = products;
-              this.performFilter(this.parentListFilter);
+              this.filterComponent.listFilter = this.productParameterService.filterBy;
           },
           (error: any) => this.errorMessage = <any>error
       );
     }
 
-    onValueChange(filter: string){
+    onValueChange(filter: string) {
+      this.productParameterService.filterBy = filter;
       this.performFilter(filter);
     }
 
